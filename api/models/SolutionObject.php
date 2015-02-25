1c6ec63473aeef690a37ccdd5fc18ee6518a6383
Serubin <serubin@serubin.net>
2015-02-25 11:55:26 -0500

    Started to add fields

--- api/models/SolutionObject.php
+++ api/models/SolutionObject.php
@@ -2,16 +2,37 @@
 /******************************************************************************
 * SolutionObject.php
  * @author Michael Shullick, Solomon Rubin
- * 31 January 2015
+ * 20 Febuary 2015
  * This is a file that will describe the standards used for mindcloud.
  * All lines will fit with 80 columns. 
  *****************************************************************************/
 
-class SolutionObject.php
+
+/*
+CREATE TABLE `solutions` (
+  `id` int(11) unsigned NOT NULL,
+  `pid` int(11) unsigned NOT NULL,
+  `shorthand` varchar(40) NOT NULL,
+  `title` varchar(160) NOT NULL,
+  `description` text NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `creator` int(11) unsigned NOT NULL,
+  `status` tinyint(2) NOT NULL DEFAULT '0'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+*/
+class SolutionObject{
 
 	private $_mysqli;
 
 	// member vars
+	public $id;
+	public $problemId;
+	public $shorthand; // Short hand title/url
+	public $title;
+	public $description; // TODO strip tags on submit to filter out html
+	public $created;
+	public $creator;
+	public $status;
 	// TODO
 	// TODO: content handlers
 	// TODO: activity
