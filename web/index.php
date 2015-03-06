a7fdb49a8a91dc1f24319b70f82429d1a8f8321f
Serubin <serubin@serubin.net>
2015-03-06 11:46:25 -0500

    finished updating file locations

--- web/index.php
+++ web/index.php
@@ -4,14 +4,14 @@
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>Mindcloud</title>
-		<link rel="stylesheet" href="/web/assets/css/master.css" />
+		<link rel="stylesheet" href="/assets/css/master.css" />
 
 		
-		<script type="text/javascript" src="/web/assets/js/frameworks.js"></script>
+		<script type="text/javascript" src="/assets/js/frameworks.js"></script>
 		<!-- JS app files -->
-		<script type="text/javascript" src="/web/assets/js/apps.js"></script>
+		<script type="text/javascript" src="/assets/js/apps.js"></script>
 		<!-- JS includes -->
-		<script type="text/javascript" src="/web/assets/js/includes.js"></script>
+		<script type="text/javascript" src="/assets/js/includes.js"></script>
 	</head>
 	<body>
 		<!-- top bar -->
