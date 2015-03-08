16b377a7cdf93d8ee738fe150eb5892905a7f316
Solomon Rubin <seru@sheepbook.student.rit.edu>
2015-03-08 13:32:17 -0400

    Added initial problem page

---
+++ web/pages/problem.php
@@ -0,0 +1,14 @@
+<div class="row" id="header">
+	<div class="large-8 small-10 columns" id="title">
+	</div>
+	<div class="large-4 small-2" id="creators">
+	</div>
+</div>
+
+<div class="row">
+	<div class="large-8 small-10 columns" id="description">
+	</div>
+	<div class="large-4 small-2" id="discussion">
+	</div>
+</div>
+
