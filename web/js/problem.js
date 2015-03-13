e3f9423d53429580ce3ae6f807c05bf53b88856f
= <=>
2015-03-12 20:11:11 -0400

    Starting minor styling on problem Moved around logincheck in a few files

--- web/js/problem.js
+++ web/js/problem.js
@@ -56,7 +56,7 @@ function processProblem(data){
 	$("#banner #title").html(data.title);
 	$("#description").html(data.description);
 
-	$("#contributers").append("<li><span>" + data.creator.association + "</span> " + data.creator.user.first_name + " " +  data.creator.user.last_name + "</li>")
+	$("#contributers").append("<li><small>" + data.creator.association + "</small> " + data.creator.user.first_name + " " +  data.creator.user.last_name + "</li>")
 }
 
 /**
