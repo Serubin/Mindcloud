9df68de5627d9a3ec684295f4992ea974de866fc
Serubin <Serubin@serubin.net>
2015-03-31 11:59:39 -0400

    Continuing work on Notification Object. Load and create done.

--- api/models/NotificationObject.php
+++ api/models/NotificationObject.php
@@ -31,7 +31,32 @@ class NotificationObject {
 	 * loads all fields for NotificationObjects
 	 */
 	public function load(){
+		if(!isset($this->id)) {
+			throw new UserException("Unset vars: id", __FUNCTION__);
+		}
 
+		if(!$stmt = $this->_mysqli->prepare("SELECT `id`, `uid`, `url`, `message`, `time` FROM `user_notifications` WHERE `id` = ? LIMIT 1")) {
+			throw new UserException($this->_mysqli->error, __FUNCTION__);
+		}
+
+		$stmt->bind_param("i", $this->id);
+		$stmt->execute();
+		$stmt->store_result();
+
+		if($stmt->num_rows < 1) {
+			throw new UserException("No rows returned", __FUNCTION__);
+		}
+
+		$stmt->bind_result($db_id, $db_uid, $db_url, $db_message, $db_time);
+		$stmt->fetch();
+
+		$this->id = $db_id;
+		$this->uid = $db_uid;
+		$this->url = $db_url;
+		$this->message = $db_message;
+		$this->time = $db_time;
+
+		$stmt->close();
 	}
 
 	/**
@@ -39,7 +64,19 @@ class NotificationObject {
 	 * Creates a new notification
 	 */
 	public function create(){
+		if(!isset($this->uid, $this->url, $this->message)) {
+			throw new UserException("unset vars: uid, url, message, time");
+		}
 
+		if($stmt = $this->_mysqli->prepare("INSERT INTO `user_notifications` (`uid`, `url`, `message`) VALUES (?,?,?)")){
+			throw new UserException($this->_mysqli->error, __FUNCTION__);
+		}
+
+		$stmt->bind_param("iss", $this->uid, $this->url, $this->message);
+		$this->execute();
+
+		$this->id = $this->_mysqli->insert_id;
+		$stmt->close();
 	}
 
 	/**
@@ -47,6 +84,10 @@ class NotificationObject {
 	 * returns a list of ids for a notification that is attached to a user
 	 */
 	public function fetchNotifications(){
+		if(!isset($this->uid)){
+			throw new UserException("Unset vars: uid", __FUNCTION__);
+		}
 
+		//TODO
 	}
 }
