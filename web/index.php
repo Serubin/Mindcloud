f43328fbed751a8cc99a7f972155278772f74adf
Serubin <Serubin@serubin.net>
2015-04-13 12:10:22 -0400

    Added problem and solution modals to index

--- web/index.php
+++ web/index.php
@@ -32,5 +32,57 @@
 			<!-- Dynamicly Filled -->
 
 		</div><!-- end content area -->
+
+		<div id="pose_create">
+			<!-- pose-a-problem modal -->
+			<div id="pose_problem_modal" class="reveal-modal" data-reveal>
+			  <h2>Pose a problem</h2>
+			  <p class="lead">Your couch.  It is mine.</p>
+
+				<form data-abide="ajax" id="submit_problem">
+
+					<!-- title -->
+				    <div class="statement-field">
+				    	<label>problem statement
+				        	<input id="form_problem_statement" type="text" placeholder="Why can't I type with my mind yet?" required/>
+				     	</label>
+				     	<small class="error">What's your problem?</small>
+				    </div>
+
+				    <!-- category -->
+				    <div class="category">
+				    	<label>category
+				    		<select id="form_problem_cat" required aria-invalid="false">
+								<option value="">Select a category</option>
+							</select>
+						</label>
+					</div>
+
+				    <!-- description -->
+				    <div class="description-field">
+				     	<label>description
+				        	<textarea id="form_problem_desc" class="small-12 columns" placeholder="Keyboards have been around since like the 1930s..." required></textarea>
+				     	</label>
+				     	<small class="error">Please elaborate on your problem.</small>
+				    </div>
+
+					<!-- tags -->
+					<div class="tag-field">
+						<label>tags
+							<input name="tag_container" id="tag_container" value="" required data-abide-validator="tagsValid"/>
+						</label>
+					 	<small class="error">Please include at least 5 tags</small>
+					 </div>
+
+					<button type="submit" class="button btn-login">create</button>
+
+				   	<!--<a role="button" type="submit" aria-label="submit form" href="#" class="button keep-native">submit</a>-->
+				</form>
+			  <a href="#" class="close-reveal-modal keep-native">&#215;</a>
+			</div> <!-- end pose-a-problem modal -->
+			<!-- create-a-solution modal -->
+			<div id="create_solution_modal" class="reveal-modal" data-reveal>
+			</div>
+		</div>
 	</body>
 </html>
