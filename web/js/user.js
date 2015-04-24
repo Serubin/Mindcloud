d03129a22951c3c32129c0d21a2185876f0e9e9a
Serubin <Serubin@serubin.net>
2015-04-24 16:48:30 -0400

    Working on fixing 404 bug

--- web/js/user.js
+++ web/js/user.js
@@ -5,6 +5,8 @@
  * Javascript for account page
  *****************************************************************************/
 
+var user_preload_transaction = false;
+
 function user(url){
 	if(url.length == 3 && url[1].toLowerCase() == "notification"){
  		var req = new APICaller("notification", "load");
@@ -16,6 +18,8 @@ function user(url){
 			else
 				ph.pageRequest("/dashboard");
 		});
+ 	} else if(user_preload_transaction) {
+ 		// just sit tight.
  	} else {
  		ph.pageRequest("error-404", false);
  	}
@@ -23,6 +27,7 @@ function user(url){
 
  function preuser(url){
  	if(url.length == 2 && url[1].toLowerCase() == "logout"){
+ 		user_preload_transaction = true; //  avoid 404
  		log.debug("User", "Logging user out");
  		var req = new APICaller("user", "logout");
  		req.send({}, function(result){
@@ -31,26 +36,27 @@ function user(url){
  			else
  				new alertHandler("alert", "There was an error logging you out. Please try again later or contact our support");
  		});
- 	}
-
- 	if(url.length == 2 && url[1].toLowerCase() == "settings"){
+ 	} else if(url.length == 2 && url[1].toLowerCase() == "settings"){
+ 		user_preload_transaction = true // avoid 404
  		log.debug("User", "Launching settings");
  		ph.pageRequest("/user-settings", false);
  	} 
  	// Notification handling
  	else if(url.length == 3 && url[1].toLowerCase() == "notification"){
  		log.debug("User", "Processing notification");
+ 		user_preload_transaction = true; // avoid 404
 
  		var req = new APICaller("notification","update");
  		var params = {
  			id: url[2],
- 			seen: 1
+ 			read: 1
  		};
  		req.send(params, function(result){
- 			log.debug("Notifications", "Notification " + url[2] + " has been marked as seen.");
- 			log.debug("Notifications", "Marked as seen result: " + result);
+ 			log.debug("Notifications", "Notification " + url[2] + " has been marked as read.");
+ 			log.debug("Notifications", "Marked as read result: " + result);
  			console.log(notificationTopbar);
  			notificationTopbar.recount();
  		});
+
  	}
  }
