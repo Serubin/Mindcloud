d58d5761f03fb7ad794c20d4054a9ec401c0c214
Serubin <Serubin@serubin.net>
2015-04-24 17:03:33 -0400

    Changed user javascript. Now it WORKS!

--- web/js/user.js
+++ web/js/user.js
@@ -8,25 +8,17 @@
 var user_preload_transaction = false;
 
 function user(url){
-	if(url.length == 3 && url[1].toLowerCase() == "notification"){
- 		var req = new APICaller("notification", "load");
-		var params = {id: url[2]};
-		req.send(params, function(result){
-			log.debug("Notifications", "Redirecting to notifiction url: " + result.url);
-			if(result)
-				ph.pageRequest(result.url);
-			else
-				ph.pageRequest("/dashboard");
-		});
- 	} else if(user_preload_transaction) {
- 		// just sit tight.
- 	} else {
- 		ph.pageRequest("error-404", false);
- 	}
-}
 
- function preuser(url){
- 	if(url.length == 2 && url[1].toLowerCase() == "logout"){
+	// Checks for user login
+	var req = new APICaller('user', 'check');
+	req.send({}, function(result) {
+		if(!result) {
+			ph.pageRequest("/login");
+			alertHandler("alert", "Please log in.");
+		}
+	});
+
+	if(url.length == 2 && url[1].toLowerCase() == "logout"){
  		user_preload_transaction = true; //  avoid 404
  		log.debug("User", "Logging user out");
  		var req = new APICaller("user", "logout");
@@ -36,17 +28,13 @@ function user(url){
  			else
  				new alertHandler("alert", "There was an error logging you out. Please try again later or ask us for help");
  		});
- 	} else if(url.length == 2 && url[1].toLowerCase() == "settings"){
- 		user_preload_transaction = true // avoid 404
+	} else if(url.length == 2 && url[1].toLowerCase() == "settings"){
  		log.debug("User", "Launching settings");
- 		ph.pageRequest("/user-settings", false);
- 	} 
- 	// Notification handling
- 	else if(url.length == 3 && url[1].toLowerCase() == "notification"){
- 		log.debug("User", "Processing notification");
- 		user_preload_transaction = true; // avoid 404
-
- 		var req = new APICaller("notification","update");
+ 		ph.pageRequest("/user_settings", false);
+	} else if(url.length == 3 && url[1].toLowerCase() == "notification"){
+		log.info("User", "Notification handler started");
+		// Handles updating read
+		var req = new APICaller("notification","update");
  		var params = {
  			id: url[2],
  			read: 1
@@ -58,5 +46,17 @@ function user(url){
  			notificationTopbar.recount();
  		});
 
+ 		// Loads notification
+ 		var req = new APICaller("notification", "load");
+		var params = {id: url[2]};
+		req.send(params, function(result){
+			log.debug("Notifications", "Redirecting to notifiction url: " + result.url);
+			if(result)
+				ph.pageRequest(result.url);
+			else
+				ph.pageRequest("/dashboard");
+		});
+ 	} else {
+ 		ph.pageRequest("error-404", false);
  	}
- }
+}
