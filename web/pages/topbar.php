491743f5d817a10dc2a9516b86ee4403de832909
Serubin <Serubin@serubin.net>
2015-04-28 22:47:57 -0400

    Updated and optimized images

--- web/pages/topbar.php
+++ web/pages/topbar.php
@@ -1,6 +1,6 @@
 <ul class="title-area">
 	<li class="name">
-		<h1><a id="home-link" href="/dashboard"><img width="150px" class="logo-mini" src="/assets/images/logo/mindcloud_full.png"></a></h1>
+		<h1><a id="home-link" href="/dashboard"><img width="150px" class="logo-mini" src="/assets/images/logo/logo_nav.png"></a></h1>
 	</li>
 	<!-- Remove the class "menu-icon" to get rid of menu icon. Take out "Menu" to just have icon alone -->
 	<li class="toggle-topbar menu-icon"><a href="#" class="keep-native" ><span>Menu</span></a></li>
