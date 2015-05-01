21bbeb8d737ec6c9019c0ed9e0f0228453dd4cda
Serubin <Serubin@serubin.net>
2015-05-01 17:02:52 -0400

    Updated images and adding prelogin js

---
+++ web/js/error.js
@@ -0,0 +1,5 @@
+function error(){
+	// set background
+	$("#splash-background").css("background-image", randImgSelect());
+	console.log("Legal stuffs");
+}
---
+++ web/js/legal.js
@@ -0,0 +1,5 @@
+function legal(){
+	// set background
+	$("#splash-background").css("background-image", randImgSelect());
+	console.log("Legal stuffs");
+}
